6a80bfec32c3479126495f6f65ac0adaf34e86c1
harshitha-gummuluri <149387919+harshitha-gummuluri@users.noreply.github.com>
2023-11-29 23:50:29 +0530

    Add files via upload

---
+++ src/main/java/com/example/todolistapp/Model/ToDoModel.java
@@ -0,0 +1,32 @@
+package com.example.todolistapp.Model;
+
+public class ToDoModel {
+    private int id;
+    private int status;
+    private String task;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean getStatus() {
+        return getStatus();
+    }
+
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
